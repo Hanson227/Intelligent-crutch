e8aa3f5135faf995aa18aa66f11c7edf158c1d7b
Hanson227 <huangyunhan2003@gmail.com>
2022-07-26 16:46:32 +0800

    增加测试代码

---
+++ 物体分类模块/STM32测试用程序/demo1/main.c
@@ -0,0 +1,224 @@
+#include "stm32f4xx.h"
+#include "sys.h"
+#include "stdio.h"
+
+static GPIO_InitTypeDef 		GPIO_InitStructure;
+static NVIC_InitTypeDef 		NVIC_InitStructure;
+static USART_InitTypeDef 		USART_InitStructure;
+
+
+#pragma import(__use_no_semihosting_swi)
+struct __FILE { int handle; /* Add whatever you need here */ };
+FILE __stdout;
+FILE __stdin;
+
+int fputc(int ch, FILE *f) {
+	
+	USART_SendData(USART1,ch);
+	while(USART_GetFlagStatus(USART1,USART_FLAG_TXE)==RESET);
+	USART_ClearFlag(USART1,USART_FLAG_TXE);
+	
+	
+	return ch;
+}
+
+void _sys_exit(int return_code) {
+
+}
+
+void delay_us(uint32_t n)
+{
+	SysTick->CTRL = 0; 			// Disable SysTick，关闭系统定时器
+	SysTick->LOAD = (168*n)-1; // 配置计数值(168*n)-1 ~ 0
+	SysTick->VAL  = 0; 		// Clear current value as well as count flag
+	SysTick->CTRL = 5; 		// Enable SysTick timer with processor clock
+	while ((SysTick->CTRL & 0x10000)==0);// Wait until count flag is set
+	SysTick->CTRL = 0; 		// Disable SysTick	
+}
+
+void delay_ms(uint32_t n)
+{
+	while(n--)
+	{
+		SysTick->CTRL = 0; 				// Disable SysTick，关闭系统定时器
+		SysTick->LOAD = (168000)-1; 	// 配置计数值(168000)-1 ~ 0
+		SysTick->VAL  = 0; 		// Clear current value as well as count flag
+		SysTick->CTRL = 5; 		// Enable SysTick timer with processor clock
+		while ((SysTick->CTRL & 0x10000)==0);// Wait until count flag is set
+	}
+	
+	SysTick->CTRL = 0; 		// Disable SysTick	
+
+}
+
+
+
+void usart1_init(uint32_t baud)
+{
+	
+	//打开PA硬件时钟	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+	
+	
+
+	//打开串口1硬件时钟
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+
+	//配置PA9和PA10为复用功能模式
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9|GPIO_Pin_10;		//第9 10根引脚
+	GPIO_InitStructure.GPIO_Mode= GPIO_Mode_AF;	//多功能模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;	//推挽输出，增加输出电流能力。
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//高速响应
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;	//没有使能上下拉电阻
+
+	GPIO_Init(GPIOA,&GPIO_InitStructure);
+
+
+	//将PA9和PA10引脚连接到串口1的硬件
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource9,GPIO_AF_USART1);
+	GPIO_PinAFConfig(GPIOA,GPIO_PinSource10,GPIO_AF_USART1);	
+	
+	
+	
+	//配置串口1相关参数：波特率、无校验位、8位数据位、1个停止位......
+	USART_InitStructure.USART_BaudRate = baud;										//波特率
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;						//8位数据位
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;							//1个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;								//无奇偶校验
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;	//无硬件流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;					//允许收发数据
+	USART_Init(USART1, &USART_InitStructure);
+	
+	
+	//配置串口1的中断触发方法：接收一个字节触发中断
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+	
+	
+	//配置串口1的中断优先级
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	
+	
+	//使能串口1工作
+	USART_Cmd(USART1, ENABLE);
+
+
+}
+
+void usart2_init(uint32_t baud)
+{
+	
+	//打开PA硬件时钟	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+	
+	
+
+	//打开串口2硬件时钟
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+	//配置PA9和PA10为复用功能模式
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5|GPIO_Pin_6;		//第5 6根引脚
+	GPIO_InitStructure.GPIO_Mode= GPIO_Mode_AF;	//多功能模式
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;	//推挽输出，增加输出电流能力。
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;//高速响应
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;	//没有使能上下拉电阻
+
+	GPIO_Init(GPIOD,&GPIO_InitStructure);
+
+
+	//将PA9和PA10引脚连接到串口1的硬件
+	GPIO_PinAFConfig(GPIOD,GPIO_PinSource5,GPIO_AF_USART2);
+	GPIO_PinAFConfig(GPIOD,GPIO_PinSource6,GPIO_AF_USART2);	
+	
+	
+	
+	//配置串口1相关参数：波特率、无校验位、8位数据位、1个停止位......
+	USART_InitStructure.USART_BaudRate = baud;										//波特率
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;						//8位数据位
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;							//1个停止位
+	USART_InitStructure.USART_Parity = USART_Parity_No;								//无奇偶校验
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;	//无硬件流控制
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;					//允许收发数据
+	USART_Init(USART1, &USART_InitStructure);
+	
+	
+	//配置串口2的中断触发方法：接收一个字节触发中断
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
+	
+	
+	//配置串口1的中断优先级
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	
+	//使能串口1工作
+	USART_Cmd(USART2, ENABLE);
+
+}
+
+int main(void)
+{
+	
+	usart1_init(115200);
+	usart2_init(115200);
+	
+	printf("ok123");
+
+	while(1)
+	{
+		
+		
+	}
+}
+
+void USART1_IRQHandler(void)
+{
+	uint8_t d;
+	
+	//检测标志位
+	if(USART_GetITStatus(USART1,USART_IT_RXNE) == SET)
+	{
+		//接收数据
+		d=USART_ReceiveData(USART1);
+		
+		
+		//将接收到的数据，返发给PC
+		USART_SendData(USART1,d);
+		while(USART_GetFlagStatus(USART1,USART_FLAG_TXE)==RESET);	
+	
+		//清空标志位
+		USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+	}
+
+}
+
+void USART2_IRQHandler(void)
+{
+	uint8_t d;
+	printf("OK");
+	//检测标志位
+	if(USART_GetITStatus(USART2,USART_IT_RXNE) == SET)
+	{
+		//接收数据
+		d=USART_ReceiveData(USART2);
+		
+		
+		//将接收到的数据，返发给PC
+		USART_SendData(USART1,d);
+		while(USART_GetFlagStatus(USART1,USART_FLAG_TXE)==RESET);	
+		
+		
+		//清空标志位
+		USART_ClearITPendingBit(USART2,USART_IT_RXNE);
+	}
+
+}
+
+
